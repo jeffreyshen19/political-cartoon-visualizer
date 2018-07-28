5d12345e26f0ec2fa8f841e0360357d882e0e1eb
jeffreyshen19 <jshen@andover.edu>
2018-07-28 11:18:20 -0700

    image download fixed

--- parser/download-images.js
+++ parser/download-images.js
@@ -7,20 +7,28 @@ var request = require("request"),
     fs = require("fs");
 
 //Recursive function that downloads images synchronously to prevent the LoC API from failing.
-function downloadImages(data, i, j){
+function downloadImages(data, i, j, isLarge){
   var results = data[i].cartoons,
     cartoon = results[j],
-    uri = "http:" + cartoon.image_url[2];
+    uri = "http:" + cartoon.image_url[isLarge ? (cartoon.image_url.length - 1) : 0];
 
   console.log(cartoon.index);
 
   request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream("../data/cartoons/large/" + cartoon.index + ".jpg")).on("close", function(){ //Once the image is downloaded, download the next one
-      if(j + 1 < results.length) downloadImages(data, i, j + 1);
-      else if(i + 1 < data.length) downloadImages(data, i + 1, 0);
+    request(uri).pipe(fs.createWriteStream("../data/cartoons/" + (isLarge ? "large" : "small") + "/" + cartoon.index + ".jpg")).on("close", function(){ //Once the image is downloaded, download the next one
+      if(isLarge){
+        setTimeout(function () {
+          if(j + 1 < results.length) downloadImages(data, i, j + 1, isLarge);
+          else if(i + 1 < data.length) downloadImages(data, i + 1, 0, isLarge);
+        }, 100);
+      }
+      else{
+        if(j + 1 < results.length) downloadImages(data, i, j + 1, isLarge);
+        else if(i + 1 < data.length) downloadImages(data, i + 1, 0, isLarge);
+      }
     });
   });
 }
 
 var data = JSON.parse(fs.readFileSync("../data/data.json"));
-downloadImages(data, 0, 0);
+downloadImages(data, 0, 0, true);
